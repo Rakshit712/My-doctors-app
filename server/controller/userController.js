09f06545e704c9a6745d3ac3a69a176064391e5e
rakshit <rakshitudp@gmail.com>
2024-04-26 13:34:39 +0530

    profile merged into user model

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -87,12 +87,101 @@ async function logIn(req,res){
             status: "Success",
             message: "User logged in successfully",
             token: token,
-        })
+        })    
+}
+
+
+async function updateProfile(req,res){
+    const id = req.params.id;
+    const userData = req.body;
+    try {
+
+        const [isValidProfile,message] = isValiduserData(userData);
+        if(!isValidProfile){
+            return res.status(403).json({
+                status: "invaled data",
+                message
+            })
+        }
+
+
+        const profileData = userData.profile;
+        const otherFields = {};
+        
+        ['name', 'gender', 'dateOfBirth','contactNo', 'email'].forEach(field => {
+            if (userData.hasOwnProperty(field)) {
+                otherFields[field] = userData[field];
+            }
+        });
+        const updatedProfile = await User.findByIdAndUpdate(
+            id,
+            { $set: { profile: profileData } }, 
+            
+            { new: true }
+        );
+        await User.findByIdAndUpdate(
+            id,
+            { $set: otherFields },
+            { new: true }
+        );
+
+        if(updatedProfile){
+            return res.status(200).json({
+                status: "success",
+                message: "Profile updated successfully",
+                data: updatedProfile
+            })
+        }else{
+            return res.status(400).json({
+                status: "failure",
+                message: "Profile not updated",
+                data: null
+            })
+        }
+
+    } catch (err) {
+        throw {
+            statusCode: err.statusCode || 500,
+            status: err.status || "Something went wrong",
+            message: err.message || "Internal server error"
+
+        }
+    }
+
+}
+
+async function getProfile(req,res){
+    try {
+        const id  = req.params.id;
+
+        const profile = await User.findById(id).populate("profile.specialities");
+        if(profile){
+            return res.status(200).json({
+                status: "success",
+                message: "Profile fetched successfully",
+                data: profile
+            })
+        }else{
+            return res.status(400).json({
+                status:"failure",
+                message:"failed to get profile"
+            })
+        }
 
         
-    
+    } catch (err) {
+        throw {
+            statusCode: err.statusCode || 500,
+            status: err.status || "Something went wrong",
+            message: err.message || "Internal server error"
+
+        }
+    }
 }
+
 module.exports = {
     signUp: errorWrapper(signUp)
     ,logIn: errorWrapper(logIn),
+    updateProfile: errorWrapper(updateProfile),
+    getProfile: errorWrapper(getProfile),
 }
--- server/models/profileModel.js
+++ server/models/profileModel.js
@@ -1,110 +1,11 @@
 const mongoose = require("mongoose");
-const User = require("./userModel");
-
-const profileSchema = mongoose.Schema(
-    {
-        userId: {
-            type: mongoose.Schema.ObjectId,
-            ref: User,
-            required: true,
-            unique:true
-        },
-        bloodGroup:{
-            type: String,
-            required: true,
-            enum:["A+","A-","B+", "AB+", "AB-","O+","O-"]
-        },
-        address:
-            {
-            houseNo:{
-                type:String,
-            },
-            street: {
-                type:String,
-            },
-            city:{
-                type:String,
-            },
-            state:{
-                type:String,
-            },
-            pincode:{
-                type:Number,
-                validate: {
-                    validator: function(value) {
-                        return /^[1-9][0-9]{5}$/.test(value);
-                    },
-                    message: props => `${props.value} is not a valid pincode. Pincode should be a 6-digit number and should not start with zero.`
-                }
-            },
-            country:{
-                type:String,
-
-            }
-        },
-        languages:{
-            type:Array
-        },
-        bio:{
-            type:String
-        },
-        qualification:[
-            {
-            certification:{
-                type:String
-            },
-            institutionName: {
-                type:String
-            },
-            yearOfCompletion:{
-                type:String
-            },
-        }],
-        specialities : [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Speciality"
-            }
-          
-
-        ],
-        experience:[
-            {
-                position:{
-                    type:String
-                },
-                place:{
-                    type:String
-                },
-                fromYear:{
-                    type:String
-                },
-                fromMonth:{
-                    type:String
-                },
-                toYear:{
-                    type:String
-                },
-                toMonth:{
-                    type:String
-                }
-
-            }
-        ],
-        consultationFee:{
-            type:Number
-        },
-        licenceNumber:{
-            type:String,
-        },
-        experienceMonths:{
-            type:String,
-        }
-        
-        
-        
+const ProfileSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     }
-)
 
-const Profile = mongoose.model("profile",profileSchema);
+})
+
+const Profile = mongoose.model("Profile", ProfileSchema);
 module.exports = Profile;
