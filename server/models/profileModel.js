c2304ad4abdbed4e81c438a251413c8c724ac87d
rakshit <rakshitudp@gmail.com>
2024-04-29 14:12:37 +0530

    get doctors by search

--- server/models/profileModel.js
+++ server/models/profileModel.js
@@ -1,11 +1,1 @@
-const mongoose = require("mongoose");
-const ProfileSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    }
 
-})
-
-const Profile = mongoose.model("Profile", ProfileSchema);
-module.exports = Profile;
--- server/router/user.router.js
+++ server/router/user.router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signUp, logIn, getProfile, updateProfile } = require("../controller/userController");
+const { signUp, logIn, getProfile, updateProfile, filterDoctors } = require("../controller/userController");
 const { verifyToken } = require("../util/userAuthentication");
 
 const Router = express.Router();
@@ -8,5 +8,6 @@ Router.post("/register",signUp)
 Router.post("/login", logIn)
 Router.put("/profile/:id",verifyToken,updateProfile)
 Router.get("/profile/:id",verifyToken,getProfile)
+Router.get("/doctors",filterDoctors)
 
 module.exports = Router;
